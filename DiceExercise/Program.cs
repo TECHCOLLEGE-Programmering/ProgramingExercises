77ff7871515cf8750f578ca99519f0b7c0bbc5b5
lkri <lkri@techcollege.dk>
2020-01-08 12:06:32 +0100

    OOP exercise describtion and init .net project.

---
+++ DiceExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DiceExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The result for the dice roll is:");
+        }
+
+        public int RollDice(Dice d)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
